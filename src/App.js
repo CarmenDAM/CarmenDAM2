d26247416d70d0d3472ff3c6a4d76025668b6f8c
CarmenDAM <cceballose01@educantabria.es>
2021-12-14 10:42:08 +0100

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./style.css";
+import Etiqueta1 from './components/Etiqueta1';
+
+
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+     <h1> Primer Componente</h1>
+     <Etiqueta1/>
+     </div>
   );
 }
